b875c4e518b8db38b0e910e01a85f28963fd5198
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-03-15 11:55:11 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,11 @@
 def solution(n, lost, reserve):
-    _reverse = sorted(list(set(reserve)-set(lost)))
+    _reserve = sorted(list(set(reserve)-set(lost)))
     _lost = sorted(list(set(lost)-set(reserve)))
-    
-    for r in _reverse:
+
+    for r in _reserve:
         if r-1 in _lost:
             _lost.remove(r-1)
         elif r+1 in _lost:
             _lost.remove(r+1)
-    
-    return n-len(_lost)
-        
+            
+    return n - len(_lost)
