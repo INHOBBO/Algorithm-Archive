b9e4138747e2f2feec07d6aa2a1edef8fbcabb86
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-03-20 17:20:22 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.35 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,11 +1,9 @@
 def solution(n):
-    answer = ""
+    answer = ''
     
-    for i in range(1, n+1):
+    for i in range(n):
         if i % 2 == 0:
-            answer += "박"
-        else:
             answer += "수"
-        
-    
+        else:
+            answer += "박"
     return answer
