a555e15d1c29531628bbbe412e6e8ade17d0c9ec
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-03-25 18:26:25 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,11 +1,33 @@
-def solution(n, lost, reserve):
-    _reserve = list(set(reserve)-set(lost))
-    _lost = list(set(lost)-set(reserve))
+# def solution(n, lost, reserve):
+#     _reserve = list(set(reserve)-set(lost))
+#     _lost = list(set(lost)-set(reserve))
     
-    for r in _reserve:
-        if r-1 in _lost:
-            _lost.remove(r-1)
-        elif r+1 in _lost:
-            _lost.remove(r+1)
+#     for r in _reserve:
+#         if r-1 in _lost:
+#             _lost.remove(r-1)
+#         elif r+1 in _lost:
+#             _lost.remove(r+1)
             
-    return n-len(_lost)
+#     return n-len(_lost)
+
+
+def solution(n, lost, reserve):
+    answer = 0
+    std = [1] * (n+1)
+    
+    for i in lost:
+        std[i] = 0
+        
+    reserve.sort()
+    
+    for i in reserve:
+        if i in lost:
+            std[i] = 1
+        elif std[i-1] == 0:
+            std[i-1] = 1
+        elif i+1 <= n and std[i+1] == 0:
+            std[i+1] = 1
+    
+    answer = std.count(1)
+    
+    return answer-1
