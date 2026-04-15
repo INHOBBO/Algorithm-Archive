025752e86ebd42559332edc72f4964c9af95f8f1
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-04-15 09:31:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -3,13 +3,14 @@ n = int(input())
 bags = 0
 
 while n >= 0:
-    if n % 5 == 0:
-        bags += (n // 5)
-        print(bags)
-        break
-    else:
-        n -= 3
-        bags += 1
-
-    if n < 0:
-        print(-1)
+	if n % 5 == 0: # 5의 배수라면 가장 최선
+		bags += (n // 5) # 몫 연산자 사용 (n/5시 .0)
+		print(bags)
+		break
+	else: # 5의 배수가 아니라면 5의 배수가 될때까지!
+		n -= 3
+		bags += 1
+	
+	if n < 0:
+		print(-1)
+		
