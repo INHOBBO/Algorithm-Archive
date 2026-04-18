d16f242a7fa59ec253ac93a364e7c5e3267d2043
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-04-18 18:40:32 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,12 +1,12 @@
 def solution(array, commands):
     answer = []
-
+    
     for command in commands:
         i = command[0]
         j = command[1]
         k = command[2]
         
-        sorted_arr = sorted(array[i-1:j])
-        answer.append(sorted_arr[k-1])
-        
+        array_sorted = sorted(array[i-1:j])    
+        answer.append(array_sorted[k-1])
+    
     return answer
