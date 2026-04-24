693319420f0f93d642e1c215c05fd2402ffc837f
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-04-24 23:13:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.41 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,5 @@
 from collections import Counter
 
 def solution(participant, completion):
-        
-    one_per = Counter(participant)-Counter(completion)
-
-    return (list(one_per.keys()))[0]
+    loser = Counter(participant) - Counter(completion)
+    return list(loser.keys())[0]
