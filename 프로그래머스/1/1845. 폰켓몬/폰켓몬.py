db07e592d3981c7f9d0eea7167de92f3d4cfb242
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-04-24 23:24:42 +0900

    [level 1] Title: 폰켓몬, Time: 0.75 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,11 @@
 def solution(nums):
-    
-    n = len(nums) // 2
-
+    n = len(nums)//2
     species_counts = len(set(nums))
     
     return min(n, species_counts)
+
+
+
+# n마리 중 n/2마리까지 가져가도 좋음
+# 종류에 따라 번호를 붙여 구분, 같은 종류는 같으 ㄴ번호
+# 
