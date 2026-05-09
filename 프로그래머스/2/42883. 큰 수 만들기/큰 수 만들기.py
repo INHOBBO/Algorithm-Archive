eb4595a7685949ddc4524615483054d89e7e212b
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-05-10 00:23:05 +0900

    [level 2] Title: 큰 수 만들기, Time: 44.34 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,14 +1,13 @@
 def solution(number, k):
     stack = []
     
-    for digit in number:
-        
-        while stack and k > 0 and stack[-1] < digit:
+    for num in number:
+        while stack and k > 0 and stack[-1] < num:
             stack.pop()
             k -= 1
-            
-        stack.append(digit)
-        
+    
+        stack.append(num)
+    
     if k > 0:
         stack = stack[:-k]
         
