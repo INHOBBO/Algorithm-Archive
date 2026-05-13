11e6b8e6ba205bf549c7d334caf8e5029fa108ec
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-05-13 18:47:00 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.14 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,31 +1,34 @@
 def solution(new_id):
-    # 1단계: 소문자 치환
-    answer = new_id.lower()
-    
-    # 2단계: 허용되지 않은 문자 제거
-    result = ""
-    for char in answer:
-        if char.isalnum() or char in "-_.":
-            result += char
-    answer = result
-    
-    # 3단계: 마침표(.)가 2번 이상 연속되면 하나로 치환
+    answer = ''
+    # 1
+    new_id = new_id.lower()
+    # 2
+    for c in new_id:
+        if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
+            answer += c
+    # 3
     while '..' in answer:
         answer = answer.replace('..', '.')
-        
-    # 4단계: 마침표(.)가 처음이나 끝에 있으면 제거
-    answer = answer.strip('.')
     
-    # 5단계: 빈 문자열이라면 "a" 대입
-    if not answer:
-        answer = "a"
+    # 4
+    if len(answer) > 0 and answer[0] == '.':
+        answer = answer[1:]
+    if len(answer) > 0 and answer[-1] == '.':
+        answer = answer[:-1]
         
-    # 6단계: 16자 이상이면 자르고, 마지막 마침표 제거
+    # 5
+    if len(answer) == 0:
+        answer += 'a'
+    
+    # 6
     if len(answer) >= 16:
-        answer = answer[:15].rstrip('.')
+        answer = answer[:15]
+    if answer[-1] == '.':
+        answer = answer[:-1]
         
-    # 7단계: 2자 이하라면 마지막 문자를 길이가 3이 될 때까지 반복
-    while len(answer) <= 2:
-        answer += answer[-1]
-        
+    # 7
+    if len(answer) <= 2:
+        while len(answer) < 3:
+            answer += answer[-1]
+            
     return answer
