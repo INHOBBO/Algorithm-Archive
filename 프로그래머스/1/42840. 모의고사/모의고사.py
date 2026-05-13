31c6150ae144be007a1a867f12fefe13a899115c
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-05-13 15:04:14 +0900

    [level 1] Title: 모의고사, Time: 1.56 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,26 +1,23 @@
-# 1번 12345...
-# 2번 2,1 2,3 2,4 2,5....
-# 3번 3,3,1,1,2,2,4,4,5,5 ....
+# 1번 학생 채점 쭉, 몇개인지 ~
 
 def solution(answers):
-    score = [0,0,0] # 1, 2, 3번 학생 점수
-
-    student1 = [1,2,3,4,5]
-    student2 = [2,1,2,3,2,4,2,5]
-    student3 = [3,3,1,1,2,2,4,4,5,5]
-
+    answer = []
+    score = [0, 0, 0]
+    a1 = [1,2,3,4,5]
+    a2 = [2,1,2,3,2,4,2,5]
+    a3 = [3,3,1,1,2,2,4,4,5,5]
+    
     for i in range(len(answers)):
-        if answers[i] == student1[i%5]:
+        if answers[i] == a1[i%5]:
             score[0] += 1
-        if answers[i] == student2[i%8]:
+        if answers[i] == a2[i%8]:
             score[1] += 1
-        if answers[i] == student3[i%10]:
+        if answers[i] == a3[i%10]:
             score[2] += 1
     
-    answer = []
-    for idx, num in enumerate(score):
+    for idx, value in enumerate(score):
         if score[idx] == max(score):
-            answer.append(idx+1)
+            answer.append(idx + 1)
     
+
     return answer
-    
