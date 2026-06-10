b55fe5772ebdf496e209e4c4e4ae05f79929c8cc
Inho Kang <145584580+INHOBBO@users.noreply.github.com>
2026-06-10 17:52:10 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -1,8 +1,9 @@
 class Solution {
     public int solution(int age) {
         int answer = 0;
-        // age는 2022년 기준
-        answer = 2022 - age + 1;
+        if(age > 0 && age <= 120) {
+            answer = 2022 - age + 1;
+        }
         return answer;
     }
 }
